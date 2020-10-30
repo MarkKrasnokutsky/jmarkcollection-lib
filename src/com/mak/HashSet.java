19ae04c58b970193f7aace64f9ef63281dc702ca
Mark Krasnokutsky <Mark120320041234@mail.ru>
2020-10-30 19:47:58 +0500

    Корректировка класса SortedSet. Выполнено наследование класса HashSet. Убраны повторяющиеся методы. Сортировка множества перенесена из метода add() класса HashSet в метод add() класса SortedSet.

--- src/com/mak/HashSet.java
+++ src/com/mak/HashSet.java
@@ -7,7 +7,7 @@ import java.util.Random;
  * Обобщенный класс
  */
 public class HashSet {
-    private Entry[] array = new Entry[0];
+    protected Entry[] array = new Entry[0];
     private int index = -1;
 
     public HashSet() {
@@ -78,15 +78,6 @@ public class HashSet {
             }
             array[arrayTemp.length] = new Entry(value);
             index++;
-            for (int i = 0; i < length; i++) {
-                for (int j = i + 1; j < length; j++) {
-                    if (array[j].getValue() < array[i].getValue()) {
-                        int temp = array[j].getValue();
-                        array[j].setValue(array[i].getValue());
-                        array[i].setValue(temp);
-                    }
-                }
-            }
         }
         return result;
     }

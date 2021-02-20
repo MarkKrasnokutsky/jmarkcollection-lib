2616d14041068e555f0251a2eedebcdc0adc4216
Mark Krasnokutsky <Mark120320041234@mail.ru>
2021-02-20 23:15:34 +0500

    Javadoc интерфейса Collection.

--- src/com/mak/common/Collection.java
+++ src/com/mak/common/Collection.java
@@ -1,9 +1,21 @@
 package com.mak.common;
 
+/**
+ * интерфейс Collection
+ **/
 public interface Collection {
+    /**
+     * Метод получе размера коллекции
+     **/
     int size();
 
+    /**
+     * Метод проверки содержащего в коллекции
+     **/
     boolean isEmpty();
 
+    /**
+     * Метод перевода коллекции в обыкновенный массив
+     **/
     int[] toArray();
 }
